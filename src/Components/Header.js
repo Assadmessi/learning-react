aa4150765c8d7a96419c8f9c25924b9e0a3d7fbd
Assadmessi <ayehtetheinmessi@gmail.com>
2022-07-05 22:24:09 +0630

    first commit

---
+++ src/Components/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Navigation from "./navigation";
+
+function Header() {
+    return (
+        <header className="border-b p-3 flex justify-between items-center">
+            <span className="font-bold">
+                AppName
+            </span>
+
+            <Navigation/>
+        </header>
+    )
+}
+
+export default Header;
